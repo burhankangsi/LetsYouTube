13cbe485ce85a7ed205bc62af80d84b9335ff996
= <=>
2022-03-09 23:53:57 +0530

    ui changes

---
+++ bucket_api/storage.go
@@ -0,0 +1,20 @@
+package bucket_api
+
+import (
+	"fmt"
+	"context"
+	"io"
+	"net/http"
+	"log"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/S3"
+	"github.com/aws/aws-sdk-go/aws/session"	
+)
+
+type AwsStorage struct {
+
+}
+
+func saveToBucket () {
+	
+}
