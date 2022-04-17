16ee908365c0b131875e9605956ac18c6e82f2f7
Burhanuddin Kangsiwala <burhanuddinkangsiwala98@gmail.com>
2022-04-18 02:30:42 +0530

    Integrated GET api to fetch stream

--- youtube-clone/src/VideoRow.js
+++ youtube-clone/src/VideoRow.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './VideoRow.css'
+import axios from 'axios';
+import { useState } from 'react';
 
 function VideoRow({views, subs, description, timestamp, channel, title, image}) {
+//  const [video] = useState('')
+  const getVideo = () => {
+    axios.get('localhost:8000/{channelId}/{videoId}/video.ts')
+    .then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="videoRow">
+    <div className="videoRow" onClick={getVideo}>
         <img src={image} alt=""/>
         <div className="videoRow_text">
             <h3> {title} </h3>
