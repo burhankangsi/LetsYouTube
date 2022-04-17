a92e3c389d8f443e43d1f869aff6e49db1a48a5a
Burhanuddin Kangsiwala <burhanuddinkangsiwala98@gmail.com>
2022-04-18 02:27:20 +0530

    Resolved the issues in downloading video

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Cloud Hosting — Google App Engine (standard)
 Domain — GoDaddy
 
 It is hosted on https://vocal-starship-53117c.netlify.app/
+
+Access Key = AKIAVX37IPHMP7QSCGEE
+Access secret = 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
+
+Access Key2 = AKIAVX37IPHMG6BBDYWJ
+Access secret 2 = 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
--- flash_api/retrieve.go
+++ flash_api/retrieve.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -37,25 +36,35 @@ type progressWriter struct {
 	display bool
 }
 
+var channelId string
+
 func GetS3ObjectSize(bucket, item string) int64 {
+	log.Info("Getting S3 object size")
 	var curr_credentials awsCreds
 	curr_credentials.bucket = "youtube-clone-bk"
+	curr_credentials.AWS_REGION = "us-east-1"
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String(curr_credentials.AWS_REGION), Credentials: credentials.AnonymousCredentials},
 	)
 
+	log.Info("Created a new session in Get S3 object size")
+
+	fullPath := channelId + "/" + "video" + "/" + item
 	svc := s3.New(sess)
 	input := &s3.HeadObjectInput{
 		Bucket: aws.String(bucket),
-		Key:    aws.String(item),
+		Key:    aws.String(fullPath),
 	}
+	log.Infof("Checking whether file is present or not, input=%v, key=%v", input, fullPath)
 
 	result, err := svc.HeadObject(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
-			fmt.Errorf("Error getting size of file", aerr)
+			log.Infof("Got an error, %v", aerr)
+			fmt.Errorf("Error getting size of file %v", aerr)
 		} else {
-			fmt.Errorf("Error getting size of file", err)
+			log.Infof("Got an error1, %v", err)
+			fmt.Errorf("Error getting size of file %v", err)
 		}
 	}
 	return *result.ContentLength
@@ -70,17 +79,20 @@ func (pw *progressWriter) init(s3ObjectSize int64) {
 	}
 }
 
-func DownloadFromS3Bucket(bucket, path, item string) error {
+func DownloadFromS3Bucket(bucket, item string) error {
+	log.Info("Started download from S3 bucket")
+	os.Setenv("AWS_ACCESS_KEY", "AKIAVX37IPHMP7QSCGEE")
+	os.Setenv("AWS_SECRET_KEY", "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")
 
-	// os.Setenv("AWS_ACCESS_KEY", "my-key")
-	// os.Setenv("AWS_SECRET_KEY", "my-secret")
-
-	file, err := os.Create(filepath.Join(path, item))
+	//file, err := os.Create(filepath.Join(path, item))
+	file, err := os.Create(item)
 	if err != nil {
 		fmt.Printf("Error in creating file path: %v \n", err)
 		os.Exit(1)
 	}
 	defer file.Close()
+
+	log.Info("File created, %v", file)
 
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
@@ -102,16 +114,18 @@ func DownloadFromS3Bucket(bucket, path, item string) error {
 	writer.init(s3ObjectSize)
 
 	// Start the download
+	fullPath := channelId + "/" + "video" + "/" + item
 	numBytes, err1 := downloader.Download(file,
 		&s3.GetObjectInput{
 			Bucket: aws.String(bucket),
-			Key:    aws.String(path + item),
+			Key:    aws.String(fullPath),
 		})
+	log.Infof("Full path is %v", fullPath)
 	if err1 != nil {
 		log.Errorf("Error while downloading the file from bucket %v, Error is %v", item, err1)
 		return err1
 	}
-	writer.finish()
+	//writer.finish()
 	fmt.Println("Download complete", file.Name(), numBytes, "bytes")
 	return nil
 }
@@ -132,28 +146,47 @@ func GetVideoObject(w http.ResponseWriter, r *http.Request, videoId string, chan
 }
 
 func fetchFile(vid string, chanId string) (content.File, error) {
+	log.Info("Entered fetch file")
 	awsCred := awsCreds{}
+
+	os.Setenv("AWS_ACCESS_KEY", "AKIAVX37IPHMG6BBDYWJ")
+	os.Setenv("AWS_SECRET_KEY", "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")
+
 	svc := s3.New(session.New(), &aws.Config{
 		Region: aws.String("us-east-1"),
 	})
 
 	awsCred.bucket = "youtube-clone-bk"
-	//context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//defer cancel()
+
+	// resp1, err4 := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(awsCred.bucket)})
+	// if err4 != nil {
+	// 	log.Infof("Unable to list items in bucket %q, %v", awsCred.bucket, err4)
+	// }
+
+	// for _, item := range resp1.Contents {
+	// 	fmt.Println("Name:         ", *item.Key)
+	// 	fmt.Println("Last modified:", *item.LastModified)
+	// 	fmt.Println("Size:         ", *item.Size)
+	// 	fmt.Println("Storage class:", *item.StorageClass)
+	// 	fmt.Println("")
+	// }
+
+	channelId = chanId
 	params := &s3.ListObjectsInput{
 		Bucket: aws.String(awsCred.bucket),
-		Prefix: aws.String(chanId + "/" + "video" + "/"),
+		Prefix: aws.String(chanId + "/" + "video"),
 	}
 	var num string
 	var count int
-
 	var file content.File
+
 	resp, err1 := svc.ListObjects(params)
 	if err1 != nil {
 		log.Infof("Failed to list s3 objects")
 		return file, err1
 	}
 	for _, obj := range resp.Contents {
+		log.Infof("Listing s3 obj")
 		if strings.Contains(*obj.Key, vid) {
 			log.Infof("File found in S3. Key: %v", *obj.Key)
 			num = *obj.Key
@@ -165,21 +198,23 @@ func fetchFile(vid string, chanId string) (content.File, error) {
 		log.Info("Video does not exist")
 		return file, nil
 	}
-	path := "s3a://" + chanId + "/" + "video" + "/"
+	path := "s3://" + awsCred.bucket + "/" + chanId + "/" + "video" + "/"
+
+	log.Infof("Path is %v", path)
 	//item := vid + ".ts"
 	item := vid + ".mp4"
 	if count == 1 {
 		log.Infof("Video exists but json file doesn't")
 	} else {
-		var err3 error
-		file, err3 = DownloadJsonFromS3(awsCred.bucket, path, item)
-		if err3 != nil {
-			log.Infof("Error occured while downloading json file, %v", err3)
-			return file, err3
-		}
+		// var err3 error
+		// file, err3 = DownloadJsonFromS3(awsCred.bucket, path, item)
+		// if err3 != nil {
+		// 	log.Infof("Error occured while downloading json file, %v", err3)
+		// 	return file, err3
+		// }
 	}
 
-	err2 := DownloadFromS3Bucket(awsCred.bucket, path, item)
+	err2 := DownloadFromS3Bucket(awsCred.bucket, item)
 	if err2 != nil {
 		log.Errorf("Could not initiate download video from S3. Error is %v", err2)
 		return file, err2
@@ -201,6 +236,7 @@ func (pw *progressWriter) finish() {
 }
 func DownloadJsonFromS3(bucket string, path string, item string) (content.File, error) {
 
+	log.Info("Now, starting to download json")
 	sess, err1 := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})
